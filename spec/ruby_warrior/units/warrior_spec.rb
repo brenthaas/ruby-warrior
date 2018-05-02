4c31f983f66f16447323494ab56b0ed681457e92
Brent Haas <brent.haas@bugcrowd.com>
2018-05-02 23:02:06 +1000

    updated warrior_spec to rspec3 syntax

--- spec/ruby_warrior/units/warrior_spec.rb
+++ spec/ruby_warrior/units/warrior_spec.rb
@@ -6,60 +6,89 @@ class Player
 end
 
 describe RubyWarrior::Units::Warrior do
-  before(:each) do
-    @warrior = RubyWarrior::Units::Warrior.new
+  describe 'name'
+  context 'when the warrior name is set to an empty value' do
+    before { subject.name = '' }
+
+    it 'defaults to "warrior"' do
+      expect(subject.name).to eq 'Warrior'
+    end
   end
-  
-  it "should default name to warrior" do
-    expect(@warrior.name).to eq("Warrior")
-    @warrior.name = ''
-    expect(@warrior.name).to eq("Warrior")
+
+  context 'when the name is set to a value' do
+    let(:name) { 'Joe' }
+
+    before { subject.name = name }
+
+    it 'uses the defined name' do
+      expect(subject.name).to eq name
+    end
   end
-  
-  it "should be able to set name" do
-    @warrior.name = "Joe"
-    expect(@warrior.name).to eq("Joe")
-    expect(@warrior.to_s).to eq("Joe")
+
+  it 'defaults to 20 max health' do
+    expect(subject.max_health).to eq 20
   end
-  
-  it "should have 20 max health" do
-    expect(@warrior.max_health).to eq(20)
+
+  it 'defaults to 0 points' do
+    expect(subject.score).to be_zero
   end
-  
-  it "should have 0 score at beginning and be able to earn points" do
-    expect(@warrior.score).to be_zero
-    @warrior.earn_points(5)
-    expect(@warrior.score).to eq(5)
+
+  context 'when earning points' do
+    let(:points) { 5 }
+
+    it 'ganes the earned points' do
+      expect { subject.earn_points(points) }
+        .to change(subject, :score)
+        .from(0)
+        .to(points)
+    end
   end
-  
-  it "should call player.play_turn and pass turn to player" do
-    player = double
-    player.expects(:play_turn).with('turn')
-    @warrior.stubs(:player).returns(player)
-    @warrior.play_turn('turn')
+
+  describe '#play_turn' do
+    let(:player) { double(Player, play_turn: true) }
+    let(:turn) { 'turn' }
+
+    before { allow(Player).to receive(:new).and_return player }
+
+    it 'delegates to player' do
+      subject.play_turn('turn')
+      expect(player).to have_received(:play_turn).with(turn)
+    end
   end
-  
-  it "should call Player.new the first time loading player, and return same object next time" do
-    Player.expects(:new).returns('player').times(1)
-    2.times do
-      expect(@warrior.player).to eq('player')
+
+  describe '#player' do
+    let!(:player) { class_spy(Player, new: double).as_stubbed_const }
+
+    it 'memoizes player' do
+      warrior = subject
+      expect(Player).to receive(:new).once
+      2.times { |_| warrior.player }
     end
   end
   
   it "should have an attack power of 5" do
-    expect(@warrior.attack_power).to eq(5)
+    expect(subject.attack_power).to eq(5)
   end
   
   it "should have an shoot power of 3" do
-    expect(@warrior.shoot_power).to eq(3)
+    expect(subject.shoot_power).to eq(3)
   end
   
   it "should appear as @ on map" do
-    expect(@warrior.character).to eq("@")
+    expect(subject.character).to eq("@")
   end
-  
-  it "should be able to add golem abilities which are used on base golem" do
-    @warrior.add_golem_abilities :walk!
-    expect(@warrior.base_golem.abilities.keys).to eq([:walk!])
+
+  describe 'when adding a golem ability' do
+    let(:ability) { :walk! }
+
+    before { subject.add_golem_abilities ability }
+
+    it 'has_golem?' do
+      expect(subject.has_golem?).to be true
+    end
+
+    it 'adds the ability to base_golem' do
+      expect(subject.base_golem.abilities).to have_key :walk!
+    end
   end
 end
